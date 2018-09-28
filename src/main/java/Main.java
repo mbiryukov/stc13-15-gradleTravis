4d1de202309a5ad24923c1b43488e8f9cf3975a0
MVBiryukov-teacher-stc <m.biryukov@innopolis.ru>
2018-09-28 14:39:27 +0300

    Init in develop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,15 @@
+import third.WordFinderImpl;
+import third.WordFinderUser;
+
+import java.net.MalformedURLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        WordFinderUser wordFinderUser = new WordFinderUser(new WordFinderImpl());
+        try {
+            wordFinderUser.doWork("some", "word");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
     }
 }
